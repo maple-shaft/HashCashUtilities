2f2d141a00bd7a0b73ebe77d97010fd3a4f79649
dev <Dustin@DESKTOP>
2017-01-20 18:46:56 -0500

    Adding cli commands and dispatch

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,28 @@
 module Main where
 
+import System.Environment
+import Data.List
 import HashCash
 
-main :: IO ()
-main = do 
+dispatch :: [(String, [String] -> IO ())]
+dispatch = [
+    ("generate", generate)
+  , ("validate", validate)
+  ]
+  
+generate :: [String] -> IO ()
+generate args = do
   header <- generateHeader
   putStrLn header
   return ()
+  
+validate :: [String] -> IO ()
+validate args = do
+  return ()
+
+main :: IO ()
+main = do 
+  (command:args) <- getArgs
+  let (Just action) = lookup command dispatch
+  action args
+  
